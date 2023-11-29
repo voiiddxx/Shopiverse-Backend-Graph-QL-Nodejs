d2c6be756747601d72a7d48e1ce6cc6e809a9c05
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-11-29 22:18:36 +0530

    databse integreated

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const {ApolloServer} = require("apollo-server");
 const typeDefs = require("./graphql/typedefs/typedef");
 const resolvers = require("./graphql/resolvers/index");
 require("dotenv").config();
-
+const mongoose = require("mongoose");
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context:({req}) => ({req})
 });
+
+mongoose.connect(process.env.DATABASE).then((res)=>{
+    console.log("Database Connected");
+}).catch((e)=>{
+    console.log(e);
+})
+
 
 server.listen({port:5000}).then((res)=>{
     console.log(`Server is live on ${res.url}`);
